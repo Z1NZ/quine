7eb07768784099fdc4eeb755433873d8c291f515
SegFault42 <rabougue@student.42.fr>
2017-02-05 05:27:58 +0100

    All ex ok, change comment

--- Grace/lib/Grace.c
+++ Grace/lib/Grace.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-#define STRING "#include <unistd.h>%c#include <fcntl.h>%c#include <stdio.h>%c#define STRING %c%s%c%c#define WRITE void print(int fd){dprintf(fd, STRING,10,10,10,34,STRING,34,10,10,34,34,10,10,10);};%c#define TOTO int main(){int fd = open(%c./Grace_kid.c%c, O_CREAT | O_RDWR, S_IWOTH); print(fd); return 0;}%cWRITE%cTOTO%c//Commentaire"
-#define WRITE void print(int fd){dprintf(fd, STRING,10,10,10,34,STRING,34,10,10,34,34,10,10,10);};
-#define TOTO int main(){int fd = open("./Grace_kid.c", O_CREAT | O_RDWR, S_IWOTH); print(fd); return 0;}
+#define STRING "#include <unistd.h>%c#include <fcntl.h>%c#include <stdio.h>%c#define STRING %c%s%c%c#define WRITE void print(int fd){dprintf(fd, STRING,10,10,10,34,STRING,34,10,10,34,34,10,10,10,10);};%c#define TOTO int main(){int fd = open(%c./Grace_kid.c%c, O_CREAT | O_RDWR, S_IRWXU); print(fd); return 0;}%cWRITE%cTOTO%c//Commentaire%c"
+#define WRITE void print(int fd){dprintf(fd, STRING,10,10,10,34,STRING,34,10,10,34,34,10,10,10,10);};
+#define TOTO int main(){int fd = open("./Grace_kid.c", O_CREAT | O_RDWR, S_IRWXU); print(fd); return 0;}
 WRITE
 TOTO
 //Commentaire
