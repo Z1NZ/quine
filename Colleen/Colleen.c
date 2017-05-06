ae8da1b841dcce597d5a037b827038916fb1437b
z1nz <sofiane.rabah@gmail.com>
2017-05-06 04:19:11 +0200

    Colleen.c ok

--- Colleen/Colleen.c
+++ Colleen/Colleen.c
@@ -1,4 +1,4 @@
 #include<stdio.h>
 /*tototest*/
 int toto(){return(1);}
-int main(){/*testtoto*/char*a="#include<stdio.h>%c/*tototest*/%cint toto(){return(1);}%cint main(){/*testtoto*/char*a=%c%s%c;printf(a,10,10,10,34,a,34);}";printf(a,10,10,10,34,a,34);}
+int main(){/*testtoto*/char*a="#include<stdio.h>%c/*tototest*/%cint toto(){return(1);}%cint main(){/*testtoto*/char*a=%c%s%c;printf(a,10,10,10,34,a,34);toto();}";printf(a,10,10,10,34,a,34);toto();}
