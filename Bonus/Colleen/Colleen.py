c475d7523c30ccb40c707a5c0434d289d9cfa261
z1nz <sofiane.rabah@gmail.com>
2017-05-06 04:14:12 +0200

    Colleen.py ok (call function) √

--- Bonus/Colleen/Colleen.py
+++ Bonus/Colleen/Colleen.py
@@ -2,6 +2,7 @@ def function():
 	return
 
 #comment
-string = 'def function():%c%creturn%c%c#comment%cstring = %c%s%c%cprint(string %% (10, 9, 10, 10, 10, 39, string, 39, 10, 10));%c#comment'
-print(string % (10, 9, 10, 10, 10, 39, string, 39, 10, 10));
+string = 'def function():%c%creturn%c%c#comment%cstring = %c%s%c%cprint(string %% (10, 9, 10, 10, 10, 39, string, 39, 10, 10, 10));%cfunction()%c#comment'
+print(string % (10, 9, 10, 10, 10, 39, string, 39, 10, 10, 10));
+function()
 #comment
