fe79e697baee70dddf4a4a78d22a5ccde76bce90
SegFault42 <rabougue@student.42.fr>
2017-07-26 17:51:04 +0200

    all ok

---
+++ Bonus/Sully/Sully_2.py
@@ -0,0 +1,8 @@
+import sys, os
+i = 1;
+file = open("Sully_%d.py" % (i), "w");
+i -= 1
+str = 'import sys, os%ci = %d;%cfile = open(%cSully_%%d.py%c %% (i), %cw%c);%ci -= 1%cstr = %c%s%c%cfile.write(str %% (10, i, 10, 34, 34, 34, 34, 10, 10, 39, str, 39, 10, 10, 10, 34, 34, 10))%cfile.close();%cif (i > -1):os.system(%cpython Sully_%%d.py%c %% (i + 1))%c'
+file.write(str % (10, i, 10, 34, 34, 34, 34, 10, 10, 39, str, 39, 10, 10, 10, 34, 34, 10))
+file.close();
+if (i > -1):os.system("python Sully_%d.py" % (i + 1))
